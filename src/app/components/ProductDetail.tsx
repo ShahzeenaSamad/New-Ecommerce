38e381f84e7ab7598a130789b9bea0b92d5af8d1
ShahzeenaSamad <164982173+ShahzeenaSamad@users.noreply.github.com>
2025-01-15 07:52:28 +0500

    Ecommerce

--- src/app/components/ProductDetail.tsx
+++ src/app/components/ProductDetail.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";  
-import { useCart } from "../components/cartContext"; 
+import { useCart } from "./cartContext"; 
 import Image from "next/image";
 import Link from "next/link";
 
